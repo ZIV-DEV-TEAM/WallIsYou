f8d5f04e0a8e73a2097a0b1446d80c9cc6d988be
alecs000 <alecs.2004fed@gmail.com>
2023-03-29 23:02:06 +0300

    Loop and mute

--- Assets/Scripts/Sound/SoundService.cs
+++ Assets/Scripts/Sound/SoundService.cs
@@ -1,15 +1,25 @@
+using DG.Tweening;
 using System;
+using System.Threading;
+using UI;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundService : MonoBehaviour
 {
     private const string Music = "Music";
     private const string Effects = "Effects";
     [SerializeField] private AudioMixerGroup audioMixerGroup;
+    [SerializeField] private int muteValue = -50;
+    [SerializeField] private Pause pause;
+    [SerializeField] private Button pauseButton;
+    private bool _isPaused;
 
     private void Start()
     {
+        pause.Initialize(UnMute);
+        pauseButton.onClick.AddListener(Mute);
         if (PlayerPrefs.HasKey(Music))
             audioMixerGroup.audioMixer.SetFloat(Music, PlayerPrefs.GetInt(Music));
         if (PlayerPrefs.HasKey(Effects))
@@ -19,10 +29,22 @@ public class SoundService : MonoBehaviour
     private void Update()
     {
         audioMixerGroup.audioMixer.GetFloat(Music, out var musicValue);
-        if ((int)musicValue != PlayerPrefs.GetInt(Music))
-            audioMixerGroup.audioMixer.SetFloat(Music, musicValue);
+        if (_isPaused)
+            audioMixerGroup.audioMixer.SetFloat(Music, muteValue);
+        else if ((int)musicValue != PlayerPrefs.GetInt(Music))
+        {
+            audioMixerGroup.audioMixer.SetFloat(Music, PlayerPrefs.GetInt(Music));
+        }
         audioMixerGroup.audioMixer.GetFloat(Music, out var effectValue);
         if ((int)effectValue != PlayerPrefs.GetInt(Effects))
             audioMixerGroup.audioMixer.SetFloat(Effects, PlayerPrefs.GetInt(Effects));
+    }
+    public void Mute()
+    {
+        _isPaused = true;
+    }
+    public void UnMute()
+    {
+        _isPaused = false;
     }
 }
